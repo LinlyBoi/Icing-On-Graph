b1354784633fb2bc9b50bbf876f92050094172f8
Supermjork <markehabaziz@gmail.com>
2022-06-01 17:02:58 +0200

    Created node to exist.

--- src/RecipeNode.java
+++ src/RecipeNode.java
@@ -1,3 +1,27 @@
-public class RecipeNode
-{
+public class RecipeNode {
+    private String ingredient;  // Ingredient's name
+    private double portion;     // Portion size of ingredient
+    RecipeNode pointer;         // To point at parent node
+
+    // Accessors and Mutators for fields (Except pointer's)
+    RecipeNode(String ingredient, double portion) {
+        this.ingredient = ingredient;
+        this.portion = portion;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setPortion(double portion) {
+        this.portion = portion;
+    }
+
+    public double getPortion() {
+        return portion;
+    }
 }
