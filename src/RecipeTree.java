4e23bba87e0a4d698c39b54c39e11883326ab082
Libkyy <libkyy@e.email>
2022-06-01 17:57:43 +0200

    Mining off camera

--- src/RecipeTree.java
+++ src/RecipeTree.java
@@ -1,3 +1,62 @@
 public class RecipeTree
+
 {
+    private RecipeNode root;
+
+    public RecipeTree()
+    {
+        root = null;
+    }
+
+    public RecipeNode getRoot()
+    {
+        return root;
+    }
+
+    public void setRoot(RecipeNode root)
+    {
+        this.root = root;
+    }
+
+    public void DFS(String value)
+    {
+        DFS(root, value);
+    }
+    public void DFS(RecipeNode node, String value)
+    {
+        if(node == null)
+        {
+            System.out.println("Node not found");
+            return;
+        }
+        if(node.getIngredient().equals(value))
+        {
+            System.out.println(node.getIngredient() + " " + node.getPortion());
+        }
+        for(RecipeNode child : node.getChildren())
+        {
+            DFS(child, value);
+        }
+    }
+
+
+    public void addNode(RecipeNode parent, RecipeNode child)
+    {
+        if (parent == null)
+        {
+            root = child;
+        }
+        else
+        {
+            parent.addChild(child);
+        }
+    }
+
+    public void printTree()
+    {
+        if (root != null)
+        {
+            root.printNode();
+        }
+    }
 }
